e6fd04244ed63aa24cb27ca514ca896a9df461f7
hi-mut <muthiahzzzz@gmail.com>
2024-06-21 18:46:00 +0700

    import qgis2web files
    gtw lah sy nyerah

---
+++ data/cleanedhaltesbycleaned_2.js
@@ -0,0 +1,1 @@
+var json_cleanedhaltesbycleaned_2 = {"type":"FeatureCollection","name":"cleanedhaltesbycleaned_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
